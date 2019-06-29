a2d0beee7de469bf8faa36270f1f5f2f8c8bcb10
Sixl-Daniel <sixl.daniel@googlemail.com>
2019-06-29 13:36:42 +0200

    Add footer link

--- includes/doc-bottom.php
+++ includes/doc-bottom.php
@@ -1,7 +1,7 @@
 <?php if(!defined('ROOT')) die("Kein direkter Zugriff erlaubt."); ?>
 <footer class="page-footer">
         <div class="container">
-            <p class="copyright">© <?= date("Y"); ?> Daniel Sixl</p>
+            <p class="copyright">© <?= date("Y"); ?> — <a href="https://github.com/Sixl-Daniel" target="_blank">Daniel Sixl</a></p>
             <p class="legal"><a href="/legal">Impressum & Datenschutz</a></p>
         </div>
 </footer>
